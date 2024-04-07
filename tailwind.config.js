6426a86554665e0da2243b6954fee5d4a2dfc593
Eniola <73790276+Eniola001@users.noreply.github.com>
2024-04-07 21:43:30 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px"
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+        lightGreen: 'rgb(1, 152, 1)',
+        darkGreen: 'rgba(1, 152, 1, 0.7)',
+        veryLightGreen: 'rgb(0, 250, 0)'
+      },
+
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      }
+    },
+
+  },
+  plugins: [],
+}
+
